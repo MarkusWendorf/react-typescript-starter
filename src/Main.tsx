e6363038d8ebad014193655981b50ea1ac1e6a15
Markus Wendorf <markuswendorf@web.de>
2018-10-28 23:18:58 +0100

    initial

---
+++ src/Main.tsx
@@ -0,0 +1,17 @@
+import * as React from "react";
+import "./main.scss";
+
+interface Props {
+
+}
+
+const Main = (props: Props) => {
+
+    return (
+      <div className="main-panel">
+        <h1>Hello World</h1>
+      </div>
+    );
+};
+
+export default Main;
